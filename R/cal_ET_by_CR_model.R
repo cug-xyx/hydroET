5d8a2727812b3432c91223dc073187d055e8ac2e
cug-xyx <987569314@qq.com>
2022-05-23 12:15:32 +0800

    upload cal_Ep_max function

--- R/cal_ET_by_CR_model.R
+++ R/cal_ET_by_CR_model.R
@@ -4,6 +4,15 @@
 # 3. 风速     U
 # 4. 净辐射   Rn
 
+inverse <- function (f,
+                    lower = -100,
+                    upper = 100) {
+  function (y) uniroot((function (x) f(x) - y),
+                       lower = lower, upper = upper,
+                       tol = 1e-05 # iterative accuracy
+  )$root
+}
+
 cal_gma <- function(Pa = NULL,
                     lambda = NULL) {
   if (is.null(lambda)) lambda = 2.45  # latent heat of vaporization [MJ kg-1]
@@ -21,5 +30,75 @@ cal_es <- function(Tair) {
   # saturation vapour pressure at the air temperature Tair [kPa]
   es = 0.6108 * exp((17.27 * Tair) / (Tair + 237.3))
 }
+
+
+cal_delta <- function(Tair) {
+  dlt = 4098 * (0.6108 * exp((17.27 * Tair)/(Tair + 237.3)))/(Tair + 237.3)^2
+  return(dlt)
+}
+
+
+cal_U10_to_U2 <- function(U10) {
+  U2 = U10 * 4.87/log(67.8 * 10 - 5.42)
+  return(U2)
+}
+
+
+cal_Twb <- function(Td,
+                    Ta,
+                    gma = NULL) {
+  if (is.null(gma)) gma = cal_gma()
+
+  es_Td = cal_es(Td) # es at dew-point temperature [kPa]
+
+  # function
+  # 0.6108 * exp((17.27 * Twb) / (Twb + 237.3)) + gma * Twb = es_Td + gma * Ta
+
+  # solve using the inverse function
+  func <- function(Twb) 0.6108 * exp((17.27 * Twb) / (Twb + 237.3)) + gma * Twb
+  y = es_Td + gma * Ta
+
+  inv <- inverse(func, 0.0001, 100)
+
+  sapply(y, FUN = inv)
+}
+
+cal_Tdry <- function(Twb,
+                     gma = NULL) {
+  if (is.null(gma)) gma = cal_gma()
+
+  Tdry = Twb + cal_es(Twb) / gma
+
+  return(Tdry)
+}
+
+
+cal_Ep_max <- function(Tdry,
+                       Rn,
+                       U2,
+                       G = NULL,
+                       gma = NULL) {
+  if(is.null(gma)) gma = cal_gma() # gamma
+
+  dlt_Tdry = cal_delta(Tdry) # delta of Tdry
+  es_Tdry  = cal_es(Tdry)    # es at dry environment temperature [deg]
+  fu = 2.6 * (1 + 0.54 * U2) # empirical wind function [mm d-1 kPa-1]
+
+  # available energy
+  if (is.null(G)) {
+    energy = Rn
+  } else (
+    energy = Rn - G
+  )
+
+  Ep_max = dlt_Tdry * energy/(dlt_Tdry+gma) + gma/(dlt_Tdry+gma) * fu * es_Tdry
+  return(Ep_max)
+}
+
+
+
+
+
+
 
 
