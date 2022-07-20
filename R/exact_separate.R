2a90443f15eed79a1a39ac9ef323b4c99c0cb757
cug-xyx <987569314@qq.com>
2022-07-20 17:59:52 +0800

    fix exact_separate function

--- R/exact_separate.R
+++ R/exact_separate.R
@@ -29,24 +29,31 @@ exact_separate <- function(d, .col_name, .col_value,
   .col_name  = enquo(.col_name)
   .col_value = enquo(.col_value)
 
-  goal_d = d %>% select(!!.col_name, !!.col_value)
+  goal_d = d %>% select(!!.col_name, !!.col_value) %>% mutate(ID = (1:nrow(.)))
 
   goal_d %<>%
     bind_cols(
-      apply(., 1, FUN = function(single_row) {
-        tibble(raw_colNm = single_row[1], raw_value = single_row[2]) %>%
-          tidyr::separate(raw_value,
-                   into = (.$raw_colNm %>% stringr::str_split(.col_name_sep) %>% unlist()),
-                   sep = .col_value_sep, remove = .remove,
-                   convert = .convert, extra = .extra, fill = .fill, ...) %>%
-          select(-raw_colNm)
-      }) %>% purrr::map_dfr(~.x)
+      goal_d %>%
+        group_nest(!!.col_name) %>%
+        apply(., 1, FUN = function(single_row) {
+          tibble(raw_colNm = single_row[[1]],
+                 raw_value = single_row[[2]][[1]],
+                 ID        = single_row[[2]][[2]]) %>%
+          tidyr::separate(
+            raw_value,
+            into = (.$raw_colNm %>% stringr::str_split(.col_name_sep) %>% unlist() %>% unique()),
+            sep = .col_value_sep, remove = .remove,
+            convert = .convert, extra = .extra, fill = .fill, ...) %>%
+            select(-raw_colNm)
+      }) %>% purrr::map_dfr(~.x) %>% arrange(ID) %>% select(-ID)
     ) %>% select(-!!.col_value, -!!.col_name)
 
   bind_cols(d, goal_d)
 }
 
-# tibble(FORMAT = c('B:C', 'A:B:C', 'B:C:D', 'A:C', 'A:B'),
-#        value = c('2:3', '1:2:3', '2:3:4', '1:3', '1:2'),
+# tibble(FORMAT = c('B:C', 'A:B:C', 'B:C:D', 'A:C', 'A:B', 'A:B'),
+#        value = c('2:3', '1:2:3', '2:3:4', '1:3', '1:2', '1:2'),
 #        tes = 1) %>%
+#   exact_separate(.col_name = FORMAT, .col_value = value, .convert = T)
+# tibble(FORMAT = rep('A:B', 10000), value = rep('2:3', 10000)) %>%
 #   exact_separate(.col_name = FORMAT, .col_value = value)
--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,2 @@
 utils::globalVariables(c('.', 'P', 'tile_point', 'X', 'x', 'xmin', 'raw_value',
-                         'raw_colNm'))
+                         'raw_colNm', 'ID'))
